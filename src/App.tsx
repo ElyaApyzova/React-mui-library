44511f6c136f8a4743b2657bc99191247c5eb533
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-02-16 22:57:18 +0300

    switch

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { MuiButton } from './components/MuiButton';
 import { MuiTypography } from './components/MuiTypography';
 import { MuiTextField } from './components/MuiTextField';
 import { MuiSelect } from './components/MuiSelect';
+import { MuiRadioButton } from './components/MuiRadioButton';
+import { MuiCheckbox } from './components/MuiCheckbox';
+import { MuiSwitch } from './components/MuiSwitch';
+import { MuiRating } from './components/MuiRating';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <MuiButton />
       <MuiTextField />
       <MuiSelect />
+      <MuiRadioButton />
+      <MuiCheckbox />
+      <MuiSwitch />
+      <MuiRating />
     </div>
   );
 }
