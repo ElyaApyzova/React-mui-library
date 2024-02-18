2fad17cf168ae213afd336159ec224a7313b71a6
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-02-18 23:48:23 +0300

    card

--- src/components/MuiCard.tsx
+++ src/components/MuiCard.tsx
@@ -1,7 +1,26 @@
-import { Box } from "@mui/material";
+import { Box, Card,  CardContent, Typography,CardActions, Button, CardMedia } from "@mui/material";
+
 
 export const MuiCard = () => {
     return (
-        <Box></Box>
+        <Box width='300px'>
+         <Card>
+            <CardMedia
+            component='img'
+            height='140'
+            image="https://images.unsplash.com/photo-1708183698996-301224359c03?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="unsplash image" />
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='div'>
+                React
+                </Typography>
+                <Typography variant="body2" color='text.secondary'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In officia omnis consectetur? Asperiores, sit, reprehenderit nemo obcaecati expedita, veritatis hic officia repellat cupiditate corporis tenetur dolor veniam! Labore, distinctio pariatur.</Typography>  
+            </CardContent>
+            <CardActions>
+                <Button size='small'>Share</Button>
+                <Button size='small'>Learn more</Button>
+            </CardActions>
+         </Card>
+        </Box>
     )
 }
---
+++ src/components/MuiImageList.tsx
@@ -0,0 +1,7 @@
+import { Stack } from "@mui/material";
+
+export const MuiImageList = () => {
+    return (
+        <div>MuiImageList</div>
+    )
+}
