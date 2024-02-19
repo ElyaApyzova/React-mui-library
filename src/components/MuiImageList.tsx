6324cf8e714d840c97a5474a03dae74f93adb64f
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-02-19 23:56:27 +0300

    ImageList

--- src/components/MuiImageList.tsx
+++ src/components/MuiImageList.tsx
@@ -1,7 +1,101 @@
-import { Stack } from "@mui/material";
+import { Stack, ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
 
 export const MuiImageList = () => {
     return (
-        <div>MuiImageList</div>
+        <Stack spacing={4}>
+         <ImageList
+            sx={{ width: 500, height: 450 }}
+            cols={3}
+            rowHeight={164}>
+                {
+                    itemData.map(item => (
+                        <ImageListItem key={item.img}>
+                           <img src={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2`} alt={item.title} loading="lazy" /> 
+                        </ImageListItem>
+                    ))
+                }
+         </ImageList>
+         <ImageList
+         variant="woven"
+            sx={{ width: 500, height: 450 }}
+            cols={3}
+            gap={8}>
+                {
+                    itemData2.map(item => (
+                        <ImageListItem key={item.img}>
+                           <img src={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2`} alt={item.title} loading="lazy" /> 
+                        </ImageListItem>
+                    ))
+                }
+         </ImageList>
+         <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
+         <ImageList
+         variant="masonry"
+            cols={3}
+            gap={8}>
+                {
+                    itemData2.map(item => (
+                        <ImageListItem key={item.img}>
+                           <img src={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2`} alt={item.title} loading="lazy" /> 
+                        </ImageListItem>
+                    ))
+                }
+         </ImageList>
+         </Box>
+        </Stack>
     )
 }
+
+const itemData = [
+    {
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+{
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+{
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+{
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+{
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+{
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+]
+
+const itemData2 = [
+    {
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+{
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+{
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+{
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+{
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+{
+    img:'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Breakfast'
+},
+]
