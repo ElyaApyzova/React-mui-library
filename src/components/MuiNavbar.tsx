dd07691115af0acf1689bcddcd80ec10167d842b
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-02-20 23:31:51 +0300

    Menu

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -1,12 +1,17 @@
 import { AppBar, IconButton, Stack, Toolbar,Typography, Button, Menu, MenuItem } from "@mui/material";
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 import { useState } from "react";
+import  KeyboardArrowDownIcon  from "@mui/icons-material/KeyboardArrowDown";
 
 
 export const MuiNavbar = () => {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-    const handleClick = (even) => {
-        
+    const open = Boolean(anchorEl)
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+    }
+    const handleClose = () => {
+        setAnchorEl(null)
     }
     return (
         <AppBar position="static">
@@ -21,12 +26,28 @@ export const MuiNavbar = () => {
                 <Button color='inherit'>Features</Button>
                 <Button color='inherit'>Pricing</Button>
                 <Button color='inherit'>About</Button>
-                <Button color='inherit' id="resources-button">Resources</Button>
+                <Button color='inherit' 
+                id="resources-button" 
+                onClick={handleClick} 
+                aria-controls={open ? 'resources-menu' : undefined} aria-haspopup='true'
+                aria-expanded={open ? 'true' : undefined}
+                endIcon={<KeyboardArrowDownIcon />}>Resources</Button>
                 <Button color='inherit'>Login</Button>
             </Stack>
-            <Menu id="resources-menu" anchorEl={anchorEl}>
-               <MenuItem>Blog</MenuItem>
-               <MenuItem>Podcast</MenuItem>
+            <Menu id="resources-menu" anchorEl={anchorEl} open={open} MenuListProps={{
+                'aria-labelledby': 'resources-button',
+            }}
+            onClose={handleClose}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+            }}
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}>
+               <MenuItem onClick={handleClose}>Blog</MenuItem>
+               <MenuItem onClick={handleClose}>Podcast</MenuItem>
             </Menu>
          </Toolbar>
         </AppBar>
