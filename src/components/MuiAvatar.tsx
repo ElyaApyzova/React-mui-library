c2230c4d3ca1af583a44e2a52d8e1a892d14784a
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-03-18 23:37:04 +0300

    completed

--- src/components/MuiAvatar.tsx
+++ src/components/MuiAvatar.tsx
@@ -1,7 +1,29 @@
-
+import { Stack, Avatar, AvatarGroup} from '@mui/material'
 
 export const MuiAvatar = () => {
     return (
-        <div></div>
+        <Stack spacing={4}>
+        <Stack direction='row' spacing={1}>
+         <Avatar sx={{ bgcolor: 'primary.light' }}>BW</Avatar>
+         <Avatar sx={{ bgcolor: 'success.light' }}>CK</Avatar>
+        </Stack>
+
+        <Stack direction='row' spacing={1}>
+        <AvatarGroup max={3}>
+         <Avatar sx={{ bgcolor: 'primary.light' }}>BW</Avatar>
+         <Avatar sx={{ bgcolor: 'success.light' }}>CK</Avatar>
+         <Avatar
+           src=''
+           alt='Jane Doe' />
+           <Avatar
+           src=''
+           alt='John Doe' />
+           </AvatarGroup>
+        </Stack>
+        <Stack direction='row' spacing={1}>
+         <Avatar variant='square' sx={{ bgcolor: 'primary.light', width: 48, height: 48 }}>BW</Avatar>
+         <Avatar variant='rounded' sx={{ bgcolor: 'success.light', width: 48, height: 48 }}>CK</Avatar>
+        </Stack>
+        </Stack>
     )
 }
--- src/components/MuiSpeedDial.tsx
+++ src/components/MuiSpeedDial.tsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material";
+import CopyIcon from '@mui/icons-material/FileCopyOutlined'
+import PrintIcon from '@mui/icons-material/Print'
+import ShareIcon from '@mui/icons-material/Share'
+import EditIcon from '@mui/icons-material/Edit'
 export const MuiSpeedDial = () => {
-    return (
-        <div>MuiSpeedDial</div>
-    )
-}
+  return (
+    <SpeedDial
+      ariaLabel="Navigation speed dial"
+      sx={{ position: "absolute", bottom: 16, right: 16 }}
+      icon={<SpeedDialIcon  openIcon={<EditIcon /> }/>}
+    >
+        <SpeedDialAction icon={<CopyIcon />} tooltipTitle='Copy' />
+        <SpeedDialAction icon={<PrintIcon />} tooltipTitle='Print' />
+        <SpeedDialAction icon={<ShareIcon />} tooltipTitle='Share' />
+    </SpeedDial>
+  );
+};
