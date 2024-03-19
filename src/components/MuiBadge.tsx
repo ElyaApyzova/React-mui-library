d4f734bc9d13a0248707ffb3326c95823e07a603
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-03-19 23:40:04 +0300

    Chip

--- src/components/MuiBadge.tsx
+++ src/components/MuiBadge.tsx
@@ -1,7 +1,21 @@
-import { Stack } from "@mui/material";
+import { Stack, Badge } from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail"
 
 export const MuiBadge = () => {
     return (
-        <Stack>MuiBadge</Stack>
+        <Stack spacing={2} direction="row">
+            <Badge badgeContent={5} color="primary">
+                <MailIcon />
+            </Badge>
+            <Badge badgeContent={0} color="secondary" showZero>
+                <MailIcon />
+            </Badge>
+            <Badge badgeContent={100} color="primary" max={999}>
+                <MailIcon />
+            </Badge>
+            <Badge variant="dot"  color="primary" invisible={true}>
+                <MailIcon />
+            </Badge>
+        </Stack>
     )
 }
--- src/components/MuiList.tsx
+++ src/components/MuiList.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { Box, List, ListItem, ListItemAvatar, ListItemIcon, ListItemText, Avatar, ListItemButton, Divider} from '@mui/material';
+import MailIcon from "@mui/icons-material/Mail"
+
 
 export const MuiList = () => {
     return (
-        <div>List</div>
+        <Box sx={{ width: '400px', bgcolor: '#efefef'}}>
+          <List>
+            <ListItem disablePadding>
+                <ListItemButton>
+                <ListItemIcon>
+                    <ListItemAvatar>
+                    <Avatar>
+                    <MailIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    </ListItemIcon>
+                <ListItemText primary='List item 1' />
+                </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem>
+            <ListItemIcon>
+            <ListItemAvatar>
+                    <Avatar>
+                    <MailIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    </ListItemIcon>
+                <ListItemText primary='List item 2' secondary='Secondary text' />
+            </ListItem>
+            <Divider />
+            <ListItem>
+            <ListItemIcon>
+            <ListItemAvatar>
+                    <Avatar>
+                    <MailIcon />
+                    </Avatar>
+                    </ListItemAvatar>
+                    </ListItemIcon>
+                <ListItemText primary='List item 3' />
+            </ListItem>
+          </List>
+        </Box>
     )
 }
