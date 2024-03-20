f75aee0b4553478c82cb5cdf767e1f72edf9dcf2
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-03-20 23:44:08 +0300

    completed

--- src/components/MuiChip.tsx
+++ src/components/MuiChip.tsx
@@ -1,8 +1,38 @@
-import React from "react";
-
+import { Stack, Chip, Avatar } from "@mui/material";
+import FaceIcon from "@mui/icons-material/Face";
+import { useState } from "react";
 
 export const MuiChip = () => {
-    return (
-        <div>Chip</div>
-    )
-}
+    const [chips, setChips] = useState(['Chip 1', 'Chip 2', 'Chip 3'])
+    const handleDelete = (chipToDelete: string) => {
+        setChips(chips => chips.filter((chip) => chip !== chipToDelete))
+    }
+  return (
+    <Stack direction="row" spacing={1}>
+      <Chip label="Chip" color="primary" size="small" icon={<FaceIcon />} />
+      <Chip
+        label="Chip outlined"
+        color="secondary"
+        size="small"
+        variant="outlined"
+        avatar={<Avatar>V</Avatar>}
+      />
+      <Chip
+        label="Click"
+        color="success"
+        size="small"
+        onClick={() => alert("Clicked")}
+      />
+      <Chip
+        label="Delete"
+        color="error"
+        size="small"
+        onClick={() => alert("Clicked")}
+        onDelete={() => alert("Delete handler called")}
+      />
+      {chips.map((chip) => (
+        <Chip key={chip} label={chip} onDelete={() => handleDelete(chip)} />
+      ))}
+    </Stack>
+  );
+};
