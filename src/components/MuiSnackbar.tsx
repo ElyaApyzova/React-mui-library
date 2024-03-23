2856aa3ad28a252ff0181796e947c8e0472f8ba6
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-03-23 23:47:31 +0300

    completed

--- src/components/MuiSnackbar.tsx
+++ src/components/MuiSnackbar.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { Snackbar, Button, Alert, AlertProps } from "@mui/material"
+import React, { useState, forwardRef } from 'react'
+
+const SnackbarAlert = forwardRef<HTMLDivElement, AlertProps>(
+    function SnackbarAlert(props, ref) {
+        return <Alert elevation={6} ref={ref} {...props}/>
+
+    }
+)
 
 export const MuiSnackbar = () => {
+    const [open, setOpen] = useState(false)
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) =>{
+        if(reason === 'clickaway') {
+            return
+        }
+        setOpen(false)
+    }
     return (
-        <div>Snackbar</div>
+        <>
+         <Button onClick={() => setOpen(true)}>Submit</Button>
+         <Snackbar
+         message='Form submitted successfully!'
+         autoHideDuration={4000} 
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          />
+        </>
     )
 }
