1d8a44b7a962fe53c1f6b8280c1306b9cf5c22bf
ElyaApyzova <elnura.apyzova.ch@gmail.com>
2024-03-25 23:28:47 +0300

    completed

--- src/components/MuiProgress.tsx
+++ src/components/MuiProgress.tsx
@@ -1,7 +1,15 @@
-import { Stack } from '@mui/material'
+import { Stack, CircularProgress, LinearProgress } from '@mui/material'
 
 export const MuiProgress = () => {
     return (
-        <Stack spacing={2}>MuiProgress</Stack>
+        <Stack spacing={2}>
+            <CircularProgress />
+            <CircularProgress color='success' />
+            <CircularProgress variant='determinate' value={60} />
+
+            <LinearProgress />
+            <LinearProgress color='success' />
+            <LinearProgress variant='determinate' value={60} />
+        </Stack>
     )
 }
